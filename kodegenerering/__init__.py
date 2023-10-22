fc035a38e61780d4f1b6dbd961ac366e149af7b7
Stefan Hagen <stefan@hagen.link>
2023-10-22 12:58:30 +0200

    Bumped impl version for release 2023.10.22

--- kodegenerering/__init__.py
+++ kodegenerering/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2022.12.29+parent.e3f9dff2'
-# [[[end]]] (checksum: 209379cf9a1621be6558f1162d66808d)
+__version__ = '2023.10.22+parent.c09a0094'
+# [[[end]]] (checksum: 051ba19f776c8a716b930eef483277eb)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
